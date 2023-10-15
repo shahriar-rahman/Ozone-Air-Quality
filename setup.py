b513fa3fec0ae6987ef5c6a8e157789c50b32711
Shahriar Rahman <shahriar.rahman10@northsouth.edu>
2023-10-15 22:31:30 +0600

    Add Project Files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Data Analysis of the air pollution dataset from the U.S. Environmental Protection Agency (EPA).',
+    author='Shahriar Rahman',
+    license='MIT',
+)
